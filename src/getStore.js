b4efd95251839be7d0edb494e819e8c22cf0d2c2
Kirk Freiheit <kirk.freiheit@gmail.com>
2021-06-02 19:54:48 -0400

    Exercise 1: add a saga with delay effect

--- src/getStore.js
+++ src/getStore.js
@@ -12,6 +12,9 @@ import { getQuery } from './utility'
 import { reducer } from './combineReducers';
 import { defaultState } from './defaultState'
 
+import createSagaMiddleware from 'redux-saga'
+import { initSagas } from './initSagas'
+
 const stateTransformer = (state) => {
     if (Iterable.isIterable(state)) return state.toJS();
     else return state;
@@ -22,7 +25,10 @@ const logger = createLogger({
 });
 
 export const getStore = ()=>{
-    const middleWares = [thunk];
+    const sagaMiddleware = createSagaMiddleware();
+    console.info('createSagaMiddleware')
+
+    const middleWares = [sagaMiddleware, thunk];
     if (getQuery()['logger']) { middleWares.push(logger)}
     const composables = [applyMiddleware(...middleWares)]
     const enhancer = compose(
@@ -33,5 +39,8 @@ export const getStore = ()=>{
         defaultState,
         enhancer
     );
+    
+    initSagas(sagaMiddleware);
+    console.info('initSagas')
     return store;
 };
